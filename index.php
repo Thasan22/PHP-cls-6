a09db80aa5f6559d1eaa511c3c23d5a0fc2fdfc5
Thasan22 <118618754+Thasan22@users.noreply.github.com>
2023-12-26 10:30:14 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>The SakiMoni LOGIN</title>
+<link rel="stylesheet" type="text/css" href="bootstrap.min.css">
+</head>
+<body>
+<section class="container mt-5">
+<div>
+<form method="post" action="login.php" class="mx-auto col-6">
+<div class="form-group my-4">
+<label for="exampleInputEmail1">Email address</label>
+<input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="email" placeholder="Enter email">
+</div>
+<div class="form-group my-4">
+<label for="exampleInputPassword1">Password</label>
+<input type="password" class="form-control" id="exampleInputPassword1" name="password" placeholder="Enter password">
+</div>
+
+<button type="submit" class="btn btn-primary">Submit</button>
+</form>
+</div>
+</section>
+
+<script src="bootstrap.bundle.min.js"></script>
+</body>
+</html>
